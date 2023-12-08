a7c2c2b3b54b896726e3218343f5062bf512a337
damianayala <damian.villarreal-ayala@menloschool.org>
2023-12-08 08:55:05 -0800

    Added display screen

---
+++ ATCSFinalProject/game.py
@@ -0,0 +1,26 @@
+import pygame
+import sys
+
+pygame.init()
+"The code used to initialize the screen was written by ChatGPT"
+width, height = 800, 600
+screen = pygame.display.set_mode((width, height))
+pygame.display.set_caption("Pygame Example")
+
+# Set up colors
+black = (0, 0, 0)
+white = (255, 255, 255)
+
+# Main game loop
+while True:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # Clear the screen
+    screen.fill(black)
+    
+    pygame.draw.rect(screen, white, (width // 2 - 50, height // 2 - 50, 100, 100))
+
+    pygame.display.update()
